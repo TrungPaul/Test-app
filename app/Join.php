5f822d26b502b1c0af0b8930217ebc6327509c0c
TrungPaul <trungmadrid@gmail.com>
2019-10-11 11:28:09 +0700

    connect database

---
+++ app/Join.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Join extends Model
+{
+    protected $table = 'joins';
+    protected $fillable = [
+        'project_id',
+        'member_id',
+        'role'
+    ];
+}
